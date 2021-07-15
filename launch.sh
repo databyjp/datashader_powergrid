b77e88a59b18759408f34e26618726894184f90d
JP Hwang <me@jphwang.com>
2021-07-15 18:36:29 +0800

    added annotations to clarify launch.sh

--- launch.sh
+++ launch.sh
@@ -6,13 +6,17 @@ export PYTHONPATH=$PYTHONPATH:`dirname "$(realpath $0)"`
 pip install --upgrade pip 
 pip install -r requirements-predeploy.txt  
 
-# # WHEN USING COILED
+# # -- WHEN USING COILED - UNHIDE THE NEXT TWO LINES --
 # python coiled_login.py  # Log into coiled using token saved as env variable
 # python coiled_create_env.py  # Create coiled env up front
-# WHEN USING LOCAL COMPUTE
+# -- END - COILED SECTION --
+
+# -- WHEN USING LOCAL COMPUTE - UNHIDE THE NEXT FOUR LINES --
 dask-scheduler &
 dask-worker 127.0.0.1:8786 --nprocs 4 --local-directory work-dir &
 python download_data.py
 python publish_data.py
+# -- END - FOR LOCAL COMPUTE --
+
 gunicorn "app:server" --timeout 120 --workers 2
 wait
